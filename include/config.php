f5937ac3f58c1d5c4f1d2394c572470407e59ee8
Nathan <nathhewett@gmail.com>
2022-03-09 09:39:54 +0000

    config

--- include/config.php
+++ include/config.php
@@ -1,2 +1,15 @@
 <?php
-////
+// connect to database and credentials
+$servername = "localhost"; // server name
+$username = "grp20224"; // username
+$password = "7oXhg1]6%-SjOmES"; // password
+$dbname = "bowser_database"; // database name
+
+// connection variable to hold credentials
+$connection = new mysqli($servername, $username, $password, $dbname);
+// confirmation if error
+if($connection->connect_error) {
+    echo $connection->connect_error;
+}
+
+?>
