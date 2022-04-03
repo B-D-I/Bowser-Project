e94e20326a05d5dbad5d840822ce720c6c181ed3
Adrian Forrester <s1902834@glos.ac.uk>
2022-04-03 15:15:50 +0100

    Add Basic Filtering to Bowser Page to only slow Deployed Bowsers
    See Filter.php for example

--- bowsers/bowsers.php
+++ bowsers/bowsers.php
@@ -22,6 +22,16 @@ if (isset($_SESSION['email'])) {
         }
     }
 }
+
+$filter = NULL;
+if(!empty($_POST['filter']))
+$filter = $_POST['filter'];
+$_SESSION["filter"] = $filter;
+if (empty($filter)){
+   $_SESSION['query'] = 'SELECT * FROM tbl_bowsers WHERE BowserID LIKE "%{TERM}%"';
+} else {
+	$_SESSION['query'] = 'SELECT * FROM tbl_bowsers WHERE BowserID LIKE "%{TERM}%" and BowserID in (select Bowser_ID from tbl_bowser_inuse) LIMIT 25';
+}
 ?>
 
 <!doctype html>
@@ -34,6 +44,7 @@ if (isset($_SESSION['email'])) {
     <link rel="stylesheet" href="../maintenance/maintenance.css" type="text/css">
     <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300&display=swap" rel="stylesheet">
     <script src='https://kit.fontawesome.com/a076d05399.js'></script>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
     <!--jQuery-->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
@@ -71,21 +82,24 @@ if (isset($_SESSION['email'])) {
 					<div class="maintenance_list">
 					<h2>Bowser Information</h2>
 						<form action="" method="POST">
-     					<?php
-							$_SESSION['query'] = 'SELECT * FROM tbl_bowsers WHERE BowserID LIKE "%{TERM}%" LIMIT 25';
-						?>
-					   	<input type="text" name="term" id="term" placeholder="Enter Bowser Serial Number...." class="form-control">
-						<script type="text/javascript">
-  						$(function() {
-     						$( "#term" ).autocomplete({
-       							source: '../include/dbsearch.php',
-     						});
-  						});
-						</script>
-						<br />
-						<input type='submit' name='submit' value='View' class='btn btn-primary'></input>
-						<?php
-						
+     					  <p>
+     					    <input type="text" name="term" id="term" placeholder="Enter Bowser Serial Number...." class="form-control">
+     					    <br />
+							<form  action="test.php" method="post">
+							<input type="checkbox" name="filter"  value="filter" onchange="this.form.submit()" <?php if(!empty($_SESSION["filter"])){echo "checked";} ?>>Show Deployed Bowsers Only
+							</form>
+							<script>  
+  							$(function() {
+     							$( "#term" ).autocomplete({
+       								source: '../include/dbsearch.php',
+     							});
+  							});
+							</script>
+     					    <br />
+							<input type='submit' name='submit' value='View' class='btn btn-primary'>
+     					    </input>
+     					    <?php
+							
 						if (isset($_POST['submit']) ) {
 								if(!empty($_POST['term'])){
 									$connection = OpenConnection();
@@ -101,7 +115,8 @@ if (isset($_SESSION['email'])) {
 								}
 							}
 						?>
-					</form>
+						
+						</form>
 					<br />
 					<?php
 						$connection = OpenConnection();
