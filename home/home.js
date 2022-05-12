ba42a63a4754faa15503d534a204b01c78f1ff54
Nathan <nathhewett@gmail.com>
2022-05-12 19:30:26 +0100

    update unit test

--- home/home.js
+++ home/home.js
@@ -81,7 +81,7 @@ function initMap() {
                         lng: position.coords.longitude,
                     };
                     infoWindow.setPosition(pos);
-                    // infoWindow.setContent("Current Location");
+                    infoWindow.setContent("Current Location");
                     infoWindow.open(map);
                     // map.setCenter(pos);
                 },
